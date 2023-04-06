4dba0540f88748186879b0718b5629b7bf83d914
sourav4125 <souravmisra4125@gmail.com>
2023-04-06 23:53:04 +0530

    mern Day-3

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -10,7 +10,7 @@ const HomeContainer = () => {
   return (
     <section
      className='grid grid-cols-1 md:grid-cols-2 gap-2 w-full ' id="home" >
-      <div className='py-2 md:-mt-48 flex-1 flex flex-col items-start  justify-center gap-6'>
+      <div className='py-2 md:-mt-46 flex-1 flex flex-col items-start  justify-center gap-6'>
        <div className=' flex items-center gap-2 justify-center bg-orange-100 px-4 py-1  rounded-full'>
         <p className='text-base text-orange-500 font-semibold'>Bike Delivery</p>
         <div className="w-8 h-8 rounded-full bg-white  overflow-hidden drop-shadow-xl">
@@ -23,9 +23,9 @@ const HomeContainer = () => {
         <button type='button' className='bg-gradient-to-br from-orange-400 to-orange-500 w-full md:w-auto px-4 py-2 rounded-lg hover:shadow-lg transition-all ease-in-out duration-100'>Order Now</button>
       </div>
       <div className='py-2  flex-1 flex items-center relative'>
-      <img src={HeroBg} className='ml-auto w-full h-auto  lg:w-460 lg:h-auto top-40' alt="hero-bg"  />
+      <img src={HeroBg} className='ml-auto w-full h-auto  lg:w-460 lg:h-auto mt-10' alt="hero-bg"  />
 
-        <div className='w-full h-full absolute top-0 left-10 flex items-center justify-center px-48 py-4 gap-4 flex-wrap drop-shadow-lg '>
+        <div className='w-full h-full  absolute top-0 left-10 flex items-center justify-center px-48 py-4 gap-4 flex-wrap drop-shadow-lg '>
            {heroData && heroData.map(n=>(
              <div key={n.id} className='lg:w-190  p-3  bg-cardOverlay backdrop-blur-l rounded-3xl flex flex-col items-center justify-center drop-shadow-lg ' >
              <img src={n.imageSrc} className='w-20 lg:w-40 -mt-10 lg:-mt-20' alt="ice-cream" />
