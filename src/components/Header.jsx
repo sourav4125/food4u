68c3c6f57d321667ad793c5f4adfbc69a68ffbb9
sourav4125 <souravmisra4125@gmail.com>
2023-04-13 21:28:29 +0530

    fsoc done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -54,7 +54,7 @@ const Header = () => {
       <div className="hidden md:flex w-full h-full items-center justify-between ">
         <Link to={"/"} className="flex items-center gap-2">
           <img src={Logo} className="w-12 object-cover" alt="LOGO" />
-          <p className="text-headingColor text-xl font-bold">City</p>
+          <p className="text-headingColor text-xl font-bold">Food 4 U</p>
         </Link>
 
         <div className="flex items-center gap-8">
@@ -148,7 +148,7 @@ const Header = () => {
         </div>
         <Link to={"/"} className="flex items-center gap-2">
           <img src={Logo} className="w-12 object-cover" alt="LOGO" />
-          <p className="text-headingColor text-xl font-bold">City</p>
+          <p className="text-headingColor text-xl font-bold">Food 4 U</p>
         </Link>
 
         <div className="relative">
