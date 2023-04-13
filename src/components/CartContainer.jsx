d4dcfe93b0a7a78eeef08130e5ad67ac601735ad
sourav4125 <souravmisra4125@gmail.com>
2023-04-13 21:50:13 +0530

    fsoc done

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -1,8 +1,8 @@
-import { motion } from "framer-motion";
 import React, { useEffect, useState } from "react";
 import { MdOutlineKeyboardBackspace } from "react-icons/md";
 import { RiRefreshFill } from "react-icons/ri";
 
+import { motion } from "framer-motion";
 import { useStateValue } from "../context/StateProvider";
 import { actionType } from "../context/reducer";
 import EmptyCart from "../img/emptyCart.svg";
@@ -36,6 +36,7 @@ const CartContainer = () => {
 
     localStorage.setItem("cartItems", JSON.stringify([]));
   };
+
   return (
     <motion.div
       initial={{ opacity: 0, x: 200 }}
@@ -48,23 +49,24 @@ const CartContainer = () => {
           <MdOutlineKeyboardBackspace className="text-textColor text-3xl" />
         </motion.div>
         <p className="text-textColor text-lg font-semibold">Cart</p>
+
         <motion.p
           whileTap={{ scale: 0.75 }}
-          className="flex items-center gap-2 p-1 px-2 my-2 bg-gray-100 rounded-md hover:shadow-md cursor-pointer text-textColor text-base"
+          className="flex items-center gap-2 p-1 px-2 my-2 bg-gray-100 rounded-md hover:shadow-md  cursor-pointer text-textColor text-base"
           onClick={clearCart}
         >
-          {" "}
           Clear <RiRefreshFill />
         </motion.p>
       </div>
 
-      {/* Bottom Section */}
+      {/* bottom section */}
       {cartItems && cartItems.length > 0 ? (
         <div className="w-full h-full bg-cartBg rounded-t-[2rem] flex flex-col">
-          {/* cart Items */}
+          {/* cart Items section */}
           <div className="w-full h-340 md:h-42 px-6 py-10 flex flex-col gap-3 overflow-y-scroll scrollbar-none">
-            {/* each & every cart Item */}
+            {/* cart Item */}
             {cartItems &&
+              cartItems.length > 0 &&
               cartItems.map((item) => (
                 <CartItem
                   key={item.id}
@@ -74,31 +76,32 @@ const CartContainer = () => {
                 />
               ))}
           </div>
-          {/* cart total section */}
 
+          {/* cart total section */}
           <div className="w-full flex-1 bg-cartTotal rounded-t-[2rem] flex flex-col items-center justify-evenly px-8 py-2">
             <div className="w-full flex items-center justify-between">
               <p className="text-gray-400 text-lg">Sub Total</p>
               <p className="text-gray-400 text-lg">$ {tot}</p>
             </div>
-
             <div className="w-full flex items-center justify-between">
               <p className="text-gray-400 text-lg">Delivery</p>
               <p className="text-gray-400 text-lg">$ 2.5</p>
             </div>
+
             <div className="w-full border-b border-gray-600 my-2"></div>
 
             <div className="w-full flex items-center justify-between">
               <p className="text-gray-200 text-xl font-semibold">Total</p>
               <p className="text-gray-200 text-xl font-semibold">
-                $ {tot + 2.5}
+                ${tot + 2.5}
               </p>
             </div>
+
             {user ? (
               <motion.button
                 whileTap={{ scale: 0.8 }}
                 type="button"
-                className="w-full p-2 rounded-full bg-gradient-to-tr from-orange-400 to-orange-600 text-gray-50 text-lg my-2 hover:shadow-lg "
+                className="w-full p-2 rounded-full bg-gradient-to-tr from-orange-400 to-orange-600 text-gray-50 text-lg my-2 hover:shadow-lg"
               >
                 Check Out
               </motion.button>
@@ -106,9 +109,9 @@ const CartContainer = () => {
               <motion.button
                 whileTap={{ scale: 0.8 }}
                 type="button"
-                className="w-full p-2 rounded-full bg-gradient-to-tr from-orange-400 to-orange-600 text-gray-50 text-lg my-2 hover:shadow-lg "
+                className="w-full p-2 rounded-full bg-gradient-to-tr from-orange-400 to-orange-600 text-gray-50 text-lg my-2 hover:shadow-lg"
               >
-                Login to CheckOut
+                Login to check out
               </motion.button>
             )}
           </div>
